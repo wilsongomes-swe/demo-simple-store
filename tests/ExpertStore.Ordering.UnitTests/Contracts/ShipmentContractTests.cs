def893c5a573bb6dc3726a1e73a9997d1af41b6e
Wilson Neto <wilson.neto@dreamdev.io>
2022-10-01 20:39:04 +0100

    WIP: starting tests

--- tests/ExpertStore.Ordering.UnitTests/Contracts/ShipmentContractTests.cs
+++ tests/ExpertStore.Ordering.UnitTests/Contracts/ShipmentContractTests.cs
@@ -10,15 +10,13 @@ using Xunit.Abstractions;
 
 namespace ExpertStore.Ordering.UnitTests.Contracts;
 
-public class ShipmentContractTests : IClassFixture<ApiFixture>
+public class ShipmentContractTests
 {
     private readonly ITestOutputHelper _output;
-    private readonly ApiFixture _api;
 
-    public ShipmentContractTests(ApiFixture api, ITestOutputHelper output)
+    public ShipmentContractTests(ITestOutputHelper output)
     {
         _output = output;
-        _api = api;
     }
 
     [Fact]
@@ -35,8 +33,8 @@ public class ShipmentContractTests : IClassFixture<ApiFixture>
 
         IPactVerifier verifier = new PactVerifier(config);
 
-        verifier.ServiceProvider("ExpertStore-Ordering", new Uri("http://localhost:5251/"))
-                .WithFileSource(new FileInfo(Path.Combine("..", "..", "..", "..", "..", "pacts", "ExpertStore-Shipment-ExpertStore-Ordering.json")))
+        verifier.ServiceProvider("ExpertStore-Ordering", new Uri("http://localhost:525"))
+                .WithFileSource(new FileInfo(Path.Combine("..", "..", "..", "..", "..", "pacts2", "ExpertStore-Shipment-ExpertStore-Ordering.json")))
                 // .WithFilter(providerState: "There is not an order with the specified ID")
                 .Verify();
     }
@@ -56,52 +54,3 @@ public class XUnitOutput : IOutput
         this.output.WriteLine(line);
     }
 }
-
-public class ApiFixture : IDisposable
-{
-    private readonly IHost server;
-
-    public Uri ServerUri { get; }
-
-    public ApiFixture()
-    {
-        this.ServerUri = new Uri("http://localhost:9222");
-
-        this.server = Host.CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(webBuilder => { 
-                webBuilder.UseUrls(this.ServerUri.ToString());
-
-                // webBuilder.UseStartup<Program>();
-            })
-            .Build();
-        this.server.Start();
-    }
-
-    public void Dispose()
-    {
-        this.server.Dispose();
-    }
-}
-
-//public class TestStartup
-//{
-//    private readonly Startup inner;
-
-//    public TestStartup(IConfiguration configuration)
-//    {
-//        this.inner = new Startup(configuration);
-//    }
-
-//    public void ConfigureServices(IServiceCollection services)
-//    {
-//        this.inner.ConfigureServices(services);
-//    }
-
-//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//    {
-//        app.UseMiddleware<ProviderStateMiddleware>()
-//           .UseMiddleware<AuthorizationTokenReplacementMiddleware>();
-
-//        this.inner.Configure(app, env);
-//    }
-//}
